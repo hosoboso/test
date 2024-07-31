092340b12ca3e4c3c12f85d145dcf998a2ab4c5b
hosoboso <139880091+hosoboso@users.noreply.github.com>
2024-07-31 19:33:36 +0900

    update

---
+++ js/social.js
@@ -0,0 +1,25 @@
+const aTag = [
+document.getElementById("x-social-button"),
+document.getElementById("facebook-social-button"),
+document.getElementById("hatena-social-button"),
+document.getElementById("line-social-button"),
+document.getElementById("bluesky-social-button"),
+document.getElementById("taittsuu-social-button"),
+]
+
+const socialName = 'hoso_boso';
+const webAddress = location.href;
+const webTitle = encodeURIComponent(document.title);
+
+const newUrl = [
+`https://x.com/share?url=${webAddress}&text=${webTitle}&via=${socialName}`,
+`https://www.facebook.com/share.php?u=${webAddress}`,
+`https://b.hatena.ne.jp/entry/${webAddress}`,
+`https://line.me/R/msg/text/?${webTitle}%0A${webAddress}`,
+`https://bsky.app/intent/compose?text=${webTitle}%20${webAddress}`,
+`https://taittsuu.com/share?text=${webTitle}%0A${webAddress}%20%40${socialName}`,
+]
+
+for (let i = 0; i < aTag.length; i++) {
+	aTag[i].setAttribute("href",newUrl[i]);
+}
---
+++ js/taittsucard.js
@@ -0,0 +1,95 @@
+const card = [
+"サメハダ",
+"すごい机",
+"壊れかけのレディオ",
+"チンジャオロースーのぬいぐるみ",
+"ログボの味",
+"ご飯とハンバーグと野菜が入っている箱",
+"雪夜こんこ",
+"沼に住む怪異",
+"うさぎ",
+"スーパーでカートを押しながらカードバトルをするガードマンのクレジットカードが描かれたカード",
+"一握りのダイヤモンド",
+"アメっぽいもの",
+"目つきの悪い土偶",
+"チーターが落っこチーター！（笑）",
+"踊るモナカ",
+"人工知能が作った人工知能",
+"とても薄い麦茶",
+"儲かっている桶屋",
+"一流シェフのキーホルダー",
+"野菜をモリモリおいしく食べるロボット",
+"銀河将棋帝国",
+"赫の精霊",
+"謎のSNS",
+"翠の精霊",
+"至高の煎餅",
+"蒼の精霊",
+"隙のないディフェンス",
+"究極時空魔法「ル・フォースガル」",
+"ビッグバンキック",
+"我が名はスザク",
+];
+
+const esa = [
+"ほかほか紅鮭弁当",
+"ジューシー超からあげ弁当",
+"シャキシャキ大盛りサラダ",
+"挽きたてホットコーヒー",
+"踊らないタイプのモナカ",
+"あんことクリームが入ったどら焼き",
+"とてつもなく食べやすい水ようかん",
+"おばあちゃんちに行くと出してくれる名前が分からないやつ",
+"サクサクとろ&#x301c;りアップルパイ",
+"大きくて厚みがあって少し柔らかいクッキー",
+"宝石のようなジェリービーンズ",
+"大切な日に食べたくなるケーキ",
+"デラビミンC",
+"ポッカリスイート",
+"IN THE エナジーゼリー",
+"とても速い麦茶",
+];
+
+const rate = ["大好物","けっこう好き","ちょっと苦手","かなり苦手",];
+
+const result = [
+[rate[0],rate[1],rate[0],rate[2],rate[3],rate[3],rate[3],rate[3],rate[1],rate[3],rate[1],"(未検証)",rate[2],rate[2],rate[2],rate[1]],
+[rate[0],rate[0],rate[2],rate[3],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],"(未検証)",rate[1],rate[1],rate[1],rate[1]],
+[rate[1],rate[3],rate[0],rate[0],rate[0],rate[0],rate[1],rate[2],rate[2],rate[2],rate[2],"(未検証)",rate[0],rate[1],rate[2],rate[0]],
+[rate[0],rate[0],rate[0],rate[1],rate[3],rate[2],rate[3],rate[1],rate[1],rate[2],rate[1],"(未検証)",rate[0],rate[0],rate[0],rate[0]],
+[rate[2],rate[2],rate[1],rate[0],rate[0],rate[0],rate[0],rate[0],rate[3],rate[2],rate[2],"(未検証)",rate[3],rate[3],rate[3],rate[3]],
+[rate[0],rate[0],rate[0],rate[0],rate[2],rate[2],rate[3],rate[3],rate[2],rate[2],rate[3],"(未検証)",rate[3],rate[1],rate[1],rate[0]],
+[rate[0],rate[0],rate[0],rate[3],rate[2],rate[2],rate[2],rate[0],rate[1],rate[0],rate[0],"(未検証)",rate[2],rate[0],rate[0],rate[0]],
+[rate[2],rate[3],rate[0],rate[1],rate[1],rate[1],rate[1],rate[0],rate[2],rate[2],rate[2],"(未検証)",rate[2],rate[2],rate[2],rate[0]],
+[rate[3],rate[3],rate[0],rate[3],rate[3],rate[2],rate[3],rate[2],rate[3],rate[3],rate[3],"(未検証)",rate[3],rate[3],rate[3],rate[1]],
+[rate[2],rate[0],rate[3],rate[0],rate[0],rate[0],rate[0],rate[0],rate[1],rate[3],rate[2],"(未検証)",rate[0],rate[0],rate[0],rate[0]],
+[rate[3],rate[3],rate[3],rate[2],rate[2],rate[2],rate[1],rate[2],rate[1],rate[1],rate[0],"(未検証)",rate[1],rate[1],rate[1],rate[3]],
+[rate[2],rate[2],rate[3],rate[3],rate[1],rate[1],rate[1],rate[1],rate[0],rate[0],rate[0],"(未検証)",rate[1],rate[1],rate[1],rate[1]],
+[rate[1],rate[0],rate[1],rate[1],rate[1],rate[0],rate[1],rate[0],rate[2],rate[3],rate[3],"(未検証)",rate[0],rate[1],rate[1],rate[0]],
+[rate[0],rate[0],rate[1],rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],"(未検証)",rate[3],rate[2],rate[3],rate[0]],
+[rate[2],rate[2],rate[3],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],"(未検証)",rate[1],rate[1],rate[1],rate[1]],
+[rate[1],rate[1],rate[1],rate[2],rate[0],rate[1],rate[1],rate[3],rate[1],rate[1],rate[1],"(未検証)",rate[1],rate[1],rate[1],rate[2]],
+[rate[0],rate[0],rate[1],rate[3],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],"(未検証)",rate[3],rate[3],rate[2],rate[0]],
+[rate[0],rate[1],rate[0],rate[0],rate[3],rate[2],rate[2],rate[2],rate[2],rate[1],rate[2],"(未検証)",rate[1],rate[1],rate[0],rate[2]],
+[rate[0],rate[0],rate[0],rate[1],rate[0],rate[0],rate[0],rate[2],rate[0],rate[0],rate[0],"(未検証)",rate[2],rate[2],rate[2],rate[3]],
+[rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],rate[0],"(未検証)",rate[2],rate[2],rate[3],rate[1]],
+[rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],rate[3],rate[2],"(未検証)",rate[0],rate[0],rate[0],rate[1]],
+[rate[3],rate[0],rate[0],rate[0],rate[1],rate[0],rate[1],rate[0],rate[2],rate[3],rate[3],"(未検証)",rate[3],rate[0],rate[0],rate[0]],
+[rate[3],rate[0],rate[3],rate[3],rate[3],rate[3],rate[2],rate[1],rate[3],rate[2],rate[3],"(未検証)",rate[0],rate[0],rate[0],rate[1]],
+[rate[1],rate[0],rate[2],rate[3],rate[1],rate[1],rate[1],rate[1],rate[0],rate[0],rate[0],"(未検証)",rate[0],rate[0],rate[0],rate[0]],
+[rate[1],rate[3],rate[0],rate[1],rate[1],rate[1],rate[1],rate[0],rate[1],rate[1],rate[2],"(未検証)",rate[1],rate[1],rate[3],rate[1]],
+[rate[0],rate[2],rate[1],rate[0],rate[1],rate[0],rate[1],rate[0],rate[2],rate[3],rate[3],"(未検証)",rate[0],rate[2],rate[1],rate[1]],
+[rate[1],rate[2],rate[1],rate[2],rate[1],rate[0],rate[0],rate[0],rate[0],rate[1],rate[0],"(未検証)",rate[3],rate[3],rate[2],rate[0]],
+[rate[0],rate[1],rate[0],rate[3],rate[2],rate[2],rate[2],rate[1],rate[1],rate[0],rate[0],"(未検証)",rate[0],rate[0],rate[0],rate[2]],
+[rate[2],rate[1],rate[3],rate[3],rate[2],rate[2],rate[1],rate[3],rate[1],rate[2],rate[2],"(未検証)",rate[3],rate[3],rate[3],rate[3]],
+[rate[2],rate[0],rate[2],rate[3],rate[3],rate[2],rate[3],rate[0],rate[1],rate[0],rate[0],"(未検証)",rate[3],rate[0],rate[1],rate[2]],
+];
+
+function cardResult() {
+	let n1 = document.cardForm.selectForm1.selectedIndex;
+	let n2 = document.cardForm.selectForm2.selectedIndex;
+	document.getElementById("view1").innerHTML = `「${card[n1]}」に「${esa[n2]}」を与えた結果は『${result[n1][n2]}』`;
+}
+
+select1.addEventListener("change", cardResult);
+select2.addEventListener("change", cardResult);
