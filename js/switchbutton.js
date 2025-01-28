6495f08901f79e175dbd73721f4afa09269e0468
hosoboso <139880091+hosoboso@users.noreply.github.com>
2025-01-28 19:55:33 +0900

    Update switchbutton.js

--- js/switchbutton.js
+++ js/switchbutton.js
@@ -5,7 +5,6 @@ hi = "hidden",
 vi = "visible";
 
 const nd = [
-	"0",
 	d.getElementById("n1").style,
 	d.getElementById("n2").style,
 	d.getElementById("n3").style,
@@ -38,12 +37,11 @@ const nd = [
 	d.getElementById("n30").style
 ];
 
-for (let i = 1; i < nd.length; i++) {
+for (let i = 0; i < nd.length; i++) {
 	nd[i].visibility = hi;
 }
 
 const cbId = [
-	"0",
 	d.getElementById("cbtn1"),
 	d.getElementById("cbtn2"),
 	d.getElementById("cbtn3"),
@@ -76,48 +74,17 @@ const cbId = [
 	d.getElementById("cbtn30"),
 ]
 
-for (let i = 1; i < cbId.length; i++) {
+for (let i = 0; i < cbId.length; i++) {
 	cbId[i].addEventListener("click", () => {vi==nd[i].visibility?nd[i].visibility=hi:nd[i].visibility=vi;});
 }
 
 d.getElementById("cbtnall").addEventListener("click", () => {
-	if (
-		(nd[1].visibility == hi)||
-		(nd[2].visibility == hi)||
-		(nd[3].visibility == hi)||
-		(nd[4].visibility == hi)||
-		(nd[5].visibility == hi)||
-		(nd[6].visibility == hi)||
-		(nd[7].visibility == hi)||
-		(nd[8].visibility == hi)||
-		(nd[9].visibility == hi)||
-		(nd[10].visibility == hi)||
-		(nd[11].visibility == hi)||
-		(nd[12].visibility == hi)||
-		(nd[13].visibility == hi)||
-		(nd[14].visibility == hi)||
-		(nd[15].visibility == hi)||
-		(nd[16].visibility == hi)||
-		(nd[17].visibility == hi)||
-		(nd[18].visibility == hi)||
-		(nd[19].visibility == hi)||
-		(nd[20].visibility == hi)||
-		(nd[21].visibility == hi)||
-		(nd[22].visibility == hi)||
-		(nd[23].visibility == hi)||
-		(nd[24].visibility == hi)||
-		(nd[25].visibility == hi)||
-		(nd[26].visibility == hi)||
-		(nd[27].visibility == hi)||
-		(nd[28].visibility == hi)||
-		(nd[29].visibility == hi)||
-		(nd[30].visibility == hi)
-	) {
-		for (let i = 1; i < nd.length; i++) {
+	if (nd.some((idN) => idN.visibility == hi)) {
+		for (let i = 0; i < nd.length; i++) {
 			nd[i].visibility = vi;
 		}
 	} else {
-		for (let i = 1; i < nd.length; i++) {
+		for (let i = 0; i < nd.length; i++) {
 			nd[i].visibility = hi;
 		}
 	}
