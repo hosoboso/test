18510cd15da0b1c3824f95e6105ee39afbc9a427
hosoboso <139880091+hosoboso@users.noreply.github.com>
2025-07-05 17:03:40 +0900

    Update switchbutton.js

--- js/switchbutton.js
+++ js/switchbutton.js
@@ -1,35 +1,42 @@
 {
+// ボタン切り替え用
 
-const d = document,
-hi = "hidden",
-vi = "visible";
+// id="n"をquerySelectorAllですべて取得（NodeList）
+const nList = document.querySelectorAll('#n');
 
-const nd = d.querySelectorAll('#n');
-
-nd.forEach((n) => {
-	n.style.visibility = hi;
+// 初期状態でvisibilityを"hidden"に設定し非表示にする
+nList.forEach((n) => {
+	n.style.visibility = "hidden";
 });
 
-const cbId = d.querySelectorAll('#cbtn');
+// id="cbtn"（ボタン）をquerySelectorAllですべて取得（NodeList）
+const cbtnList = document.querySelectorAll('#cbtn');
 
-cbId.forEach((n,index) => {
-	n.addEventListener("click", () => {
-		vi == nd[index].style.visibility
-		? nd[index].style.visibility = hi
-		: nd[index].style.visibility = vi;
+// "cbtn"ボタンを押した時、"visible"と"hidden"状態を切り替える
+// indexはquerySelectorAllの順番（インデックス）
+// "cbtn"ボタンと対応したnList（NodeList）の個々の項目と合わせるためnList[index]と指定
+cbtnList.forEach((cbtn,index) => {
+	cbtn.addEventListener("click", () => {
+		"visible" == nList[index].style.visibility
+		? nList[index].style.visibility = "hidden"
+		: nList[index].style.visibility = "visible";
 	});
 });
 
-const ndArray = Array.from(nd);
+// NodeListはArrayではないので、Array.fromを使ってArray化する
+// （この後Array.someを使いたいので）
+const nArray = Array.from(nList);
 
-d.getElementById("cbtnall").addEventListener("click", () => {
-	if (ndArray.some((x) => x.style.visibility == hi)) {
-		nd.forEach((n) => {
-			n.style.visibility = vi;
+// "cbtnall"ボタンを押した時の動作
+document.getElementById("cbtnall").addEventListener("click", () => {
+	// Array.someで「1つでも"hidden"なら」の条件を指定する
+	if (nArray.some((x) => x.style.visibility == "hidden")) {
+		nList.forEach((n) => {
+			n.style.visibility = "visible";
 		});
 	} else {
-		nd.forEach((n) => {
-			n.style.visibility = hi;
+		nList.forEach((n) => {
+			n.style.visibility = "hidden";
 		});
 	}
 });
